d000a2b63541833bee4335aade6e37fe122474ce
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-17 21:41:18 -0700

    Built out the favorites reducer

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,7 +9,7 @@ const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const { movies, deleteMovie } = props;
+    const { movies, deleteMovie, displayFavorites } = props;
     const movie = movies.find(movie=>movie.id===Number(id));
     
     const handleDeleteClick = () => {
@@ -58,7 +58,8 @@ const Movie = (props) => {
 
 const mapStateToProps = (state) => {
     return ({
-        movies: state.movies
+        movies: state.movieReducer.movies,
+        displayFavorites: state.favoritesReducer.displayFavorites
     })
 }
 
